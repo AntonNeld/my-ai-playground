46842dd15d14a8b406111fe2d13b6252a3e32601
Axel Savén Östebo <axelostebo@gmail.com>
2019-02-02 12:13:55 +0100

    The player exists and can move with the arrow keys

--- pyglet_client/main.py
+++ pyglet_client/main.py
@@ -1,19 +1,58 @@
-import draw
+import pyglet
+import math
 import requests
 import json
+from pyglet.window import key
+
+n = 0
+x = y = 128
+window = pyglet.window.Window()
+block = pyglet.resource.image('res/img/block.png')
+player = pyglet.resource.image('res/img/player.png')
 
 
-def main():
-    drawer = draw.Draw()
+@window.event
+def on_draw():
 
-    while True:
-        r = requests.get("http://127.0.0.1:5000/api/view")
-        things = json.loads(r.text)
-        print(things)
-        drawer.draw()
-        input()
-        r = requests.post("http://127.0.0.1:5000/api/step")
+    label = pyglet.text.Label('Hacker skills baby!!',
+                              font_name='Times New Roman',
+                              font_size=46,
+                              color=(n % 256, (n+100) %
+                                     256, (n+200) % 256, 255),
+                              x=window.width//2, y=window.height//2,
+                              anchor_x='center', anchor_y='center')
+
+    window.clear()
+    label.draw()
+    block.blit(x + math.cos(n/10) * 64, y + math.sin(n/10) * 64)
+    block.blit(x + math.cos(n/10 + math.pi) * 64,
+               y + math.sin(n/10 + math.pi) * 64)
+    player.blit(x, y)
 
 
-if __name__ == "__main__":
-    main()
+@window.event
+def on_key_press(symbol, modifiers):
+    global x, y, n
+
+    r = requests.get("http://127.0.0.1:5000/api/view")
+    things = json.loads(r.text)
+    print(things)
+    input()
+    r = requests.post("http://127.0.0.1:5000/api/step")
+
+    print('A key was pressed')
+    if symbol == key.RIGHT:
+        x += 32
+    elif symbol == key.LEFT:
+        x -= 32
+    elif symbol == key.UP:
+        y += 32
+    elif symbol == key.DOWN:
+        y -= 32
+
+    n += 1
+
+    print(n)
+
+
+pyglet.app.run()
