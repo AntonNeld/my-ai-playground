b5056211c6d305a65af9e35b6bebd4027eaa8455
Anton Neld <antonherrneld@gmail.com>
2019-03-09 15:26:04 +0100

    pyglet_client: Structure keybindings.

--- pyglet_client/main.py
+++ pyglet_client/main.py
@@ -8,14 +8,33 @@ FPS = 30
 
 STEP_DURATION = 0.3
 
+config = {"manual_mode": False}
+
 window = pyglet.window.Window(width=800, height=600)
 event_loop = pyglet.app.EventLoop()
 
 views = [View(step_duration=STEP_DURATION)]
 
-# Uncomment to test multiple views
+# Uncomment to test multiple views, currently of the same dungeon
 # views = [View((30, 770, 80, 260),step_duration=STEP_DURATION),
 #          View((0, 400, 300, 600),step_duration=STEP_DURATION)]
+
+
+def print_help():
+    help_string = """
+    m: toggle manual mode
+    r: reset dungeon
+    h: print this help again
+    space: step forward
+
+    If in manual mode (and the backend supports it):
+        up: move up
+        left: move left
+        right: move right
+        down: move down
+        space: do nothing
+    """
+    print(help_string)
 
 
 @window.event
@@ -27,23 +46,26 @@ def on_draw():
 
 @window.event
 def on_key_press(symbol, modifiers):
-    if symbol == key.RIGHT:
-        set_action("move_right")
-    elif symbol == key.LEFT:
-        set_action("move_left")
-    elif symbol == key.UP:
-        set_action("move_up")
-    elif symbol == key.DOWN:
-        set_action("move_down")
-    else:
-        set_action("none")
 
-    if symbol == key.R:
-        requests.put("http://127.0.0.1:5000/api/reset")
-    else:
-        requests.post("http://127.0.0.1:5000/api/step")
-    for view in views:
-        view.get_state()
+    if symbol == key.M:
+        config["manual_mode"] = not config["manual_mode"]
+        print("Manual mode: " + str(config["manual_mode"]))
+    elif symbol == key.R:
+        reset()
+    elif symbol == key.H:
+        print_help()
+
+    if config["manual_mode"]:
+        actions = {key.RIGHT: "move_right",
+                   key.LEFT: "move_left",
+                   key.UP: "move_up",
+                   key.DOWN: "move_down",
+                   key.SPACE: "none"}
+        if symbol in actions:
+            set_action(actions[symbol])
+            step()
+    elif symbol == key.SPACE:
+        step()
 
 
 @event_loop.event
@@ -55,7 +77,19 @@ def set_action(action):
     try:
         requests.put("http://127.0.0.1:5100/api/setmove", json=action)
     except requests.exceptions.ConnectionError:
-        pass
+        print("Cannot set next move. Is there a manual AI?")
+
+
+def step():
+    requests.post("http://127.0.0.1:5000/api/step")
+    for view in views:
+        view.get_state()
+
+
+def reset():
+    requests.put("http://127.0.0.1:5000/api/reset")
+    for view in views:
+        view.get_state()
 
 
 def animate(dt):
@@ -64,6 +98,7 @@ def animate(dt):
 
 
 if __name__ == "__main__":
+    print_help()
     for view in views:
         view.get_state()
     pyglet.clock.schedule_interval(animate, 1/FPS)
