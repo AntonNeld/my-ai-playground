59e8c3f1655a20d3002b560b5d3e97042db20715
Axel Savén Östebo <axelostebo@gmail.com>
2019-03-24 16:58:17 +0100

    Added exhaustive AI

---
+++ ai/ais/exhaustive.py
@@ -0,0 +1,55 @@
+from lib.perception import get_coordinates
+from lib.pathfinding import breadth_first
+
+actions = {}
+
+
+def next_move(agent, percept):
+    print("Starting up!")
+
+    if agent not in actions or not actions[agent]:
+        walls = get_coordinates(percept, "wall")
+        coins = get_coordinates(percept, "coin")
+
+        distances = {}
+        pos = list(coins) + [(0, 0)]
+
+        for coin in pos:
+            for coin2 in pos:
+                if coin is not coin2:
+                    distances[(coin, coin2)] = breadth_first(
+                        coin, coin2, walls)
+
+        actions[agent] = _helper((0, 0), coins, [], None, walls, distances)
+
+    return actions[agent].pop(0)
+
+
+def delete(agent):
+    if agent in actions:
+        del actions[agent]
+
+
+def _helper(pos, coins, path, bestpath, walls, distances):
+    print("Pos: " + str(pos) + "     Coins left: " + str(len(coins)))
+
+    if coins is None or len(coins) == 0:
+        return []
+
+    if len(coins) == 1:
+        #print("End of the line")
+        (coin,) = coins
+        return path + distances[(pos, coin)]
+
+    for coin in coins:
+        p = path + distances[(pos, coin)]
+        if bestpath is not None and len(p) >= len(bestpath):
+            #print("This path stinks!")
+            continue
+        p2 = _helper(coin, coins.difference(
+            [coin]), p, bestpath, walls, distances)
+
+        if bestpath is None or len(bestpath) > len(p2):
+            bestpath = p2
+
+    return bestpath
