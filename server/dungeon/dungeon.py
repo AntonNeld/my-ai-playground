556a7f53d897848a58460a0fde235aca645e0f96
Anton Neld <antonherrneld@gmail.com>
2020-05-22 16:56:47 +0200

    Rename 'rooms.py' to 'room.py'

--- server/dungeon/dungeon.py
+++ server/dungeon/dungeon.py
@@ -1,6 +1,6 @@
 import uuid
 
-from dungeon import rooms
+from dungeon import room
 from dungeon.ais import ManualAI
 
 
@@ -9,37 +9,37 @@ class Dungeon:
     def __init__(self):
         self._rooms = {}
 
-    def get_view(self, room):
-        return self._rooms[room].get_view(include_id=True)
+    def get_view(self, room_id):
+        return self._rooms[room_id].get_view(include_id=True)
 
-    def get_score(self, room, agents=None):
+    def get_score(self, room_id, agents=None):
         scores = []
-        for agent in self._rooms[room].get_agents():
+        for agent in self._rooms[room_id].get_agents():
             if agents is None or agent in agents:
                 scores.append({"id": agent.id, "score": agent.score})
         return scores
 
-    def get_steps(self, room):
-        return self._rooms[room].steps
+    def get_steps(self, room_id):
+        return self._rooms[room_id].steps
 
-    def step(self, room):
-        self._rooms[room].step()
+    def step(self, room_id):
+        self._rooms[room_id].step()
 
     def create_room(self, data):
-        room = uuid.uuid4().hex
-        self._rooms[room] = rooms.create_room_from_list(data)
-        return room
+        room_id = uuid.uuid4().hex
+        self._rooms[room_id] = room.create_room_from_list(data)
+        return room_id
 
-    def create_room_with_id(self, room, data):
-        self._rooms[room] = rooms.create_room_from_list(data)
+    def create_room_with_id(self, room_id, data):
+        self._rooms[room_id] = room.create_room_from_list(data)
 
-    def delete_room(self, room):
-        if room in self._rooms:
-            del self._rooms[room]
+    def delete_room(self, room_id):
+        if room_id in self._rooms:
+            del self._rooms[room_id]
 
-    def manual_set_move(self, room, agent_id, action):
+    def manual_set_move(self, room_id, agent_id, action):
         entity = next(
-            agent for agent in self._rooms[room].get_agents()
+            agent for agent in self._rooms[room_id].get_agents()
             if agent.id == agent_id)
         if isinstance(entity.ai, ManualAI):
             entity.ai.set_move(action)
