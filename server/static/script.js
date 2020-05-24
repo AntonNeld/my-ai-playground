35efd1616a0f2fcad7efe2f5fec9c051df81cd01
Anton Neld <antonherrneld@gmail.com>
2020-05-24 16:20:39 +0200

    Frontend: Split room into reusable class

---
+++ server/static/script.js
@@ -0,0 +1,32 @@
+const testRoom = [
+  { x: 0, y: 0, type: "player", ai: "pathfinder" },
+  { x: 1, y: 1, type: "block" },
+  { x: 1, y: 0, type: "coin" },
+];
+
+let room;
+
+async function init() {
+  await fetch("/api/rooms/testroom", {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(testRoom),
+  });
+  room = new Room(document.querySelector(".room-area"));
+
+  await update();
+}
+
+async function update() {
+  const response = await fetch("/api/rooms/testroom");
+  const data = await response.json();
+
+  room.setData(data);
+}
+
+async function step() {
+  await fetch("/api/rooms/testroom/step", { method: "POST" });
+  update();
+}
+
+init();
