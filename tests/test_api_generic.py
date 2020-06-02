c1dc88be039719eac91e99c2bc5dccaf1f13b3b5
Anton Neld <antonherrneld@gmail.com>
2020-06-02 19:32:20 +0200

    Make tests look a bit nicer

--- tests/test_api_generic.py
+++ tests/test_api_generic.py
@@ -60,76 +60,82 @@ class TestResource:
         return f'{parent_url}/{resource["resource"]}'
 
     @pytest.fixture(params=["post", "put", "replaced"])
-    def existing_resource(self, client, base_url, resource, request):
+    def existing_resource(self, client, base_url, example,
+                          example_two, request):
         if request.param == "post":
             resource_id = client.post(
-                base_url, json=resource["example"]).json()
+                base_url, json=example).json()
         elif request.param == "put":
             resource_id = client.put(
-                f"{base_url}/someid", json=resource["example"]).json()
+                f"{base_url}/someid", json=example).json()
         elif request.param == "replaced":
             resource_id = client.put(
-                f"{base_url}/someid", json=resource["example_two"]).json()
+                f"{base_url}/someid", json=example_two).json()
             resource_id = client.put(
-                f"{base_url}/someid", json=resource["example"]).json()
-        return {"id": resource_id, "resource": resource["example"]}
+                f"{base_url}/someid", json=example).json()
+        return {"id": resource_id, "resource": example}
+
+    @pytest.fixture
+    def example(self, resource):
+        return resource["example"]
+
+    @pytest.fixture
+    def example_two(self, resource):
+        return resource["example_two"]
 
     @pytest.fixture(params=["clean", "deleted"])
-    def nonexistent_resource(self, client, base_url, resource, request):
+    def nonexistent_resource(self, client, base_url, example, request):
         if request.param == "clean":
             resource_id = "someid"
         elif request.param == "deleted":
             resource_id = client.post(
-                base_url, json=resource["example"]).json()
+                base_url, json=example).json()
             client.delete(f"{base_url}/{resource_id}")
         return {"id": resource_id}
 
-    def test_post_resource(self, client, base_url, resource):
-        response = client.post(base_url, json=resource["example"])
+    def test_post_resource(self, client, base_url, example):
+        response = client.post(base_url, json=example)
         assert response.status_code == 200
         resource_id = response.json()
         assert is_uuid(resource_id)
 
-    def test_list_resource(self, client, base_url,
-                           resource, existing_resource):
+    def test_list_resource(self, client, base_url, existing_resource):
         response = client.get(base_url)
         assert response.status_code == 200
         resource_list = response.json()
         assert resource_list == [existing_resource["id"]]
 
-    def test_list_empty_resource(self, client, base_url,
-                                 resource, nonexistent_resource):
+    def test_list_empty_resource(self, client, base_url, nonexistent_resource):
         response = client.get(base_url)
         assert response.status_code == 200
         resource_list = response.json()
         assert resource_list == []
 
-    def test_put_resource(self, client, base_url, resource):
+    def test_put_resource(self, client, base_url, example):
         response = client.put(
-            f'{base_url}/someid', json=resource["example"])
+            f'{base_url}/someid', json=example)
         assert response.status_code == 200
         assert response.json() == 'someid'
 
     def test_replace_resource(self, client, base_url,
-                              resource, existing_resource):
+                              example_two, existing_resource):
         response = client.put(
             f'{base_url}/{existing_resource["id"]}',
-            json=resource["example_two"])
+            json=example_two)
         assert response.status_code == 200
         assert response.json() == existing_resource["id"]
 
-    def test_get_resource(self, client, base_url, resource, existing_resource):
+    def test_get_resource(self, client, base_url, existing_resource):
         response = client.get(f'{base_url}/{existing_resource["id"]}')
         assert response.status_code == 200
         assert response.json() == existing_resource["resource"]
 
-    def test_delete_resource(self, client, base_url, resource,
-                             existing_resource):
+    def test_delete_resource(self, client, base_url, existing_resource):
         response = client.delete(f'{base_url}/{existing_resource["id"]}')
         assert response.status_code == 200
 
     def test_get_nonexistent_resource(self, client, base_url,
-                                      resource, nonexistent_resource):
+                                      nonexistent_resource):
         response = client.get(f'{base_url}/{nonexistent_resource["id"]}')
         assert response.status_code == 404
         assert response.json() == {"message": "Resource not found"}
