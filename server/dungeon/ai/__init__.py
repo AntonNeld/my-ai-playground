c581986aa17fb533f788b76867bd668060154446
Anton Neld <antonherrneld@gmail.com>
2020-07-29 16:36:01 +0200

    Remove exhaustive AI
    It was not computationally feasible.

--- server/dungeon/ai/__init__.py
+++ server/dungeon/ai/__init__.py
@@ -3,8 +3,7 @@ from typing import Union
 from .pathfinder import PathfinderAI
 from .singular import SingularAI
 from .random import RandomAI
-from .exhaustive import ExhaustiveAI
 
-AI = Union[SingularAI, PathfinderAI, ExhaustiveAI, RandomAI]
+AI = Union[SingularAI, PathfinderAI, RandomAI]
 
 __all__ = ("AI",)
