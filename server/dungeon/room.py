b2cdacd535903fe56fe8ae21c84077cb9f439025
Anton Neld <antonherrneld@gmail.com>
2020-09-17 21:26:38 +0200

    Add map of entities indexed by location for improved performance

--- server/dungeon/room.py
+++ server/dungeon/room.py
@@ -1,21 +1,36 @@
 import uuid
-from typing import Dict
+from typing import Dict, Tuple
 
 from pydantic import BaseModel
 
 from errors import ResourceNotFoundError
-from dungeon.entity import Entity
+from dungeon.entity import Entity, Position
 from profiling import time_profiling
 
 
 class Room(BaseModel):
     steps: int = 0
     entities: Dict[str, Entity] = {}
+    # Private fields
+    entities_by_location: Dict[Tuple[int, int], Entity] = {}
+
+    def dict(self, exclude=None, **kwargs):
+        private_fields = {'entities_by_location'}
+        new_exclude = (exclude | private_fields if exclude is not None
+                       else private_fields)
+        return super().dict(**{'exclude': new_exclude, **kwargs})
 
     def add_entity(self, entity, entity_id=None):
         if entity_id is None:
             entity_id = uuid.uuid4().hex
+        # Replace the entity if it already exists
+        if entity_id in self.entities:
+            self.remove_entity_by_id(entity_id)
         self.entities[entity_id] = entity
+        if entity.position is not None:
+            x = entity.position.x
+            y = entity.position.y
+            self._add_entity_to_locations(entity, x, y)
         return entity_id
 
     def remove_entity(self, entity):
@@ -23,11 +38,34 @@ class Room(BaseModel):
         for entity_id, entity_object in self.entities.items():
             if entity_object == entity:
                 id_to_remove = entity_id
-        del self.entities[id_to_remove]
+        self.remove_entity_by_id(id_to_remove)
 
     def remove_entity_by_id(self, entity_id):
         if entity_id in self.entities:
+            entity = self.entities[entity_id]
+            if (entity.position is not None):
+                x = entity.position.x
+                y = entity.position.y
+                self._remove_entity_from_locations(entity, x, y)
+
             del self.entities[entity_id]
+
+    def _add_entity_to_locations(self, entity, x, y):
+        if (x, y) not in self.entities_by_location:
+            self.entities_by_location[(x, y)] = []
+        self.entities_by_location[(x, y)].append(entity)
+
+    def _remove_entity_from_locations(self, entity, x, y):
+        self.entities_by_location[(x, y)].remove(entity)
+        if len(self.entities_by_location[(x, y)]) == 0:
+            del self.entities_by_location[(x, y)]
+
+    def move_entity(self, entity, x, y):
+        old_x = entity.position.x
+        old_y = entity.position.y
+        self._remove_entity_from_locations(entity, old_x, old_y)
+        entity.position = Position(x=x, y=y)
+        self._add_entity_to_locations(entity, x, y)
 
     def list_entities(self):
         return list(self.entities.keys())
@@ -42,6 +80,11 @@ class Room(BaseModel):
         return list(filter(lambda e: all(getattr(e, key) == value
                                          for key, value in kwargs.items()),
                            self.entities.values()))
+
+    def get_entities_at(self, x, y):
+        if (x, y) in self.entities_by_location:
+            return self.entities_by_location[(x, y)]
+        return []
 
     def get_view(self, perceptor):
         if perceptor.perception is None or perceptor.position is None:
@@ -113,7 +156,7 @@ class Room(BaseModel):
                                 and entity.score is not None):
                             entity.score -= entity.actions[action].cost
 
-                    # Move and handle collisions
+                    # Move
                     dx = dy = 0
 
                     if action == "move_up":
@@ -124,47 +167,56 @@ class Room(BaseModel):
                         dx = -1
                     elif action == "move_right":
                         dx = 1
-                    new_x = entity.position.x + dx
-                    new_y = entity.position.y + dy
-                    colliding_entities = [e for e in self.get_entities(
-                    ) if e.position is not None and e.position.x == new_x
-                        and e.position.y == new_y and e != entity]
-                    if not any(map(lambda e: e.blocks_movement is True,
-                                   colliding_entities)):
-                        entity.position.x = new_x
-                        entity.position.y = new_y
-                        if entity.count_tags_score is not None:
-                            tags = {
-                                tag: 0 for tag in entity.count_tags_score.tags}
-                            for colliding_entity in colliding_entities:
-                                if colliding_entity.tags is not None:
-                                    for tag in colliding_entity.tags:
-                                        if tag in tags:
-                                            tags[tag] += 1
-                            if tags == entity.count_tags_score.tags:
-                                add_to = self.get_entities(
-                                    label=entity.count_tags_score.add_to)[0]
-                                if add_to.score is not None:
-                                    score = entity.count_tags_score.score
-                                    add_to.score += score
-                        if (entity.pickupper is not None
-                                and (entity.pickupper.mode == "auto"
-                                     or action == "pick_up")):
-                            pickups = [
-                                e for e in colliding_entities
-                                if e.pickup is not None
-                            ]
-                            for colliding_entity in pickups:
-                                self.remove_entity(colliding_entity)
-                                kind = colliding_entity.pickup.kind
-                                if kind == "item":
-                                    colliding_entity.position = None
-                                    entity.pickupper.inventory.append(
-                                        colliding_entity)
-                                elif kind == "vanish":
-                                    pass
-                                elif kind == "addScore":
-                                    added_score = colliding_entity.pickup.score
-                                    if entity.score is not None:
-                                        entity.score += added_score
+
+                    if dx != 0 or dy != 0:
+                        new_x = entity.position.x + dx
+                        new_y = entity.position.y + dy
+                        colliding_entities = [
+                            e for e in self.get_entities_at(new_x, new_y)
+                            if e is not entity
+                        ]
+                        if not any(map(lambda e: e.blocks_movement is True,
+                                       colliding_entities)):
+                            self.move_entity(entity, new_x, new_y)
+
+                    # Handle colissions
+                    overlapping_entities = [
+                        e for e in self.get_entities_at(entity.position.x,
+                                                        entity.position.y)
+                        if e is not entity
+                    ]
+                    if entity.count_tags_score is not None:
+                        tags = {
+                            tag: 0 for tag in entity.count_tags_score.tags}
+                        for overlapping_entity in overlapping_entities:
+                            if overlapping_entity.tags is not None:
+                                for tag in overlapping_entity.tags:
+                                    if tag in tags:
+                                        tags[tag] += 1
+                        if tags == entity.count_tags_score.tags:
+                            add_to = self.get_entities(
+                                label=entity.count_tags_score.add_to)[0]
+                            if add_to.score is not None:
+                                score = entity.count_tags_score.score
+                                add_to.score += score
+                    if (entity.pickupper is not None
+                            and (entity.pickupper.mode == "auto"
+                                 or action == "pick_up")):
+                        pickups = [
+                            e for e in overlapping_entities
+                            if e.pickup is not None
+                        ]
+                        for pickup in pickups:
+                            self.remove_entity(pickup)
+                            kind = pickup.pickup.kind
+                            if kind == "item":
+                                pickup.position = None
+                                entity.pickupper.inventory.append(
+                                    pickup)
+                            elif kind == "vanish":
+                                pass
+                            elif kind == "addScore":
+                                added_score = pickup.pickup.score
+                                if entity.score is not None:
+                                    entity.score += added_score
             self.steps += 1
