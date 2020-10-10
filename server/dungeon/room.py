953a44c14ac658ae7983daff61a8b05e1277bddb
Anton Neld <antonherrneld@gmail.com>
2020-10-10 17:49:33 +0200

    Don't construct entities as often
    This improves performance some. It's still not as good as it was,
    but it will become better when using ECS.

--- server/dungeon/room.py
+++ server/dungeon/room.py
@@ -156,20 +156,21 @@ class Room(BaseModel):
         my_x = perceptor.position.x
         my_y = perceptor.position.y
         entities_view = []
-        entities = self.get_entities(include_id=True)
-        for entity_id, entity in [(i, e) for i, e in entities
-                                  if e.position is not None]:
+        for entity_id in self.entity_ids:
             if entity_id == perceptor_id:
                 continue
-            other_x = entity.position.x
-            other_y = entity.position.y
+            if (entity_id not in self.position
+                    or entity_id not in self.looks_like):
+                continue
+            other_x = self.position[entity_id].x
+            other_y = self.position[entity_id].y
             max_dist = perceptor.perception.distance
             if (max_dist is not None
                     and abs(other_x-my_x) + abs(other_y-my_y) > max_dist):
                 continue
             entity_view = {"x":          other_x - my_x,
                            "y":          other_y - my_y,
-                           "looks_like": entity.looks_like}
+                           "looks_like": self.looks_like[entity_id]}
             entities_view.append(entity_view)
         percept["entities"] = entities_view
 
@@ -194,45 +195,49 @@ class Room(BaseModel):
 
     def step(self, steps=1):
         for _ in range(steps):
-            for entity_id, entity in self.get_entities(include_id=True):
+            for entity_id in list(self.entity_ids):
                 # Skip if the entity has already been removed
                 if entity_id not in self.list_entities():
                     continue
                 # Only entities with a position can act
                 # This is a proxy for not being picked up,
                 # and should be fixed.
-                if entity.position is not None:
+                if entity_id in self.position:
                     # Update AI state and find next action
-                    if entity.ai is not None:
+                    if entity_id in self.ai:
                         percept = self.get_view(entity_id)
-                        do_time_profiling = (entity.label is not None
+                        do_time_profiling = (entity_id in self.label
                                              and time_profiling.started)
-                        do_memory_profiling = (entity.label is not None
+                        do_memory_profiling = (entity_id in self.label
                                                and memory_profiling.started)
                         if do_time_profiling:
-                            time_profiling.set_context(entity.label)
+                            time_profiling.set_context(self.label[entity_id])
                         if do_memory_profiling:
-                            memory_profiling.set_context(entity.label)
-                        if hasattr(entity.ai, "update_state_percept"):
-                            entity.ai.update_state_percept(percept)
-                        action = entity.ai.next_action(percept)
-                        if hasattr(entity.ai, "update_state_action"):
-                            entity.ai.update_state_action(action)
+                            memory_profiling.set_context(self.label[entity_id])
+                        ai = self.ai[entity_id]
+                        if hasattr(ai, "update_state_percept"):
+                            ai.update_state_percept(percept)
+                        action = ai.next_action(percept)
+                        if hasattr(ai, "update_state_action"):
+                            ai.update_state_action(action)
                         if do_time_profiling:
-                            time_profiling.unset_context(entity.label)
+                            time_profiling.unset_context(self.label[entity_id])
                         if do_memory_profiling:
-                            memory_profiling.unset_context(entity.label)
+                            memory_profiling.unset_context(
+                                self.label[entity_id])
                     else:
                         action = doNothing
                     # Don't perform actions we're not allowed to
-                    if (entity.actions is None
-                            or action.action_type not in entity.actions):
+                    if (entity_id not in self.actions
+                            or action.action_type not in
+                            self.actions[entity_id]):
                         action = doNothing
                     else:
                         action_type = action.action_type
-                        if (entity.actions[action_type].cost is not None
-                                and entity.score is not None):
-                            cost = entity.actions[action_type].cost
+                        if (self.actions[entity_id][action_type].cost
+                                is not None
+                                and entity_id in self.score):
+                            cost = self.actions[entity_id][action_type].cost
                             self.score[entity_id] -= cost
                     # Move
                     dx = dy = 0
@@ -248,18 +253,22 @@ class Room(BaseModel):
                             dx = 1
 
                     if dx != 0 or dy != 0:
-                        new_x = entity.position.x + dx
-                        new_y = entity.position.y + dy
+                        new_x = self.position[entity_id].x + dx
+                        new_y = self.position[entity_id].y + dy
                         colliding_entities = [
                             self.get_entity(e) for e in
                             self.get_entities_at(new_x, new_y)
                             if e is not entity_id
                         ]
-                        if not any(map(lambda e: e.blocks_movement is not None
-                                       and not set(entity.get_tags()) & set(
-                                           e.blocks_movement.passable_for_tags
-                                       ),
-                                       colliding_entities)):
+                        if not any(
+                            map(
+                                lambda e: e.blocks_movement is not None
+                                and not set(
+                                    e.blocks_movement.passable_for_tags
+                                ) & set(
+                                    self.get_entity(entity_id).get_tags()
+                                ),
+                                colliding_entities)):
                             self.move_entity(entity_id, new_x, new_y)
 
                     # Handle colissions
@@ -270,24 +279,25 @@ class Room(BaseModel):
                         )
                         if e is not entity_id
                     ]
-                    if entity.count_tags_score is not None:
+                    if entity_id in self.count_tags_score:
                         tags = {
-                            tag: 0 for tag in entity.count_tags_score.tags}
+                            tag: 0 for tag
+                            in self.count_tags_score[entity_id].tags}
                         for overlapping_entity in overlapping_entities:
                             for tag in self.get_entity(
                                     overlapping_entity).get_tags():
                                 if tag in tags:
                                     tags[tag] += 1
-                        if tags == entity.count_tags_score.tags:
+                        if tags == self.count_tags_score[entity_id].tags:
                             add_to_id, add_to = self.get_entities(
                                 include_id=True,
-                                label=entity.count_tags_score.add_to
+                                label=self.count_tags_score[entity_id].add_to
                             )[0]
                             if add_to.score is not None:
-                                score = entity.count_tags_score.score
+                                score = self.count_tags_score[entity_id].score
                                 self.score[add_to_id] += score
-                    if (entity.pickupper is not None
-                            and (entity.pickupper.mode == "auto"
+                    if (entity_id in self.pickupper
+                            and (self.pickupper[entity_id].mode == "auto"
                                  or action.action_type == "pick_up")):
                         pickups = [
                             (e, self.get_entity(e)) for e
@@ -296,27 +306,30 @@ class Room(BaseModel):
                         ]
                         for pickup_id, pickup in pickups:
                             kind = pickup.pickup.kind
-                            if (kind == "item"
-                                    and not entity.pickupper.full_inventory()):
+                            if (
+                                kind == "item" and not
+                                self.pickupper[entity_id].full_inventory()
+                            ):
                                 self.remove_entity_by_id(pickup_id)
                                 pickup.position = None
-                                entity.pickupper.inventory.append(
+                                self.pickupper[entity_id].inventory.append(
                                     pickup)
                             elif kind == "vanish":
                                 self.remove_entity_by_id(pickup_id)
                             elif kind == "addScore":
                                 self.remove_entity_by_id(pickup_id)
                                 added_score = pickup.pickup.score
-                                if entity.score is not None:
+                                if entity_id in self.score:
                                     self.score[entity_id] += added_score
                     # Drop items (do this after collisions to not immediately
                     # pick them up again)
                     if action.action_type == "drop":
                         try:
-                            dropped_entity = entity.pickupper.inventory.pop(
-                                action.index)
-                            dropped_entity.position = entity.position.copy(
+                            inventory = self.pickupper[entity_id].inventory
+                            dropped_entity = inventory.pop(action.index)
+                            new_position = self.position[entity_id].copy(
                                 deep=True)
+                            dropped_entity.position = new_position
                             self.add_entity(dropped_entity)
                         except IndexError:
                             pass
