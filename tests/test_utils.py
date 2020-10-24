c8bcbfe3d633ea738eaf1b5e4c241a9224bf1948
Anton Neld <antonherrneld@gmail.com>
2020-10-24 17:01:12 +0200

    Removed unused test util room_from_yaml

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,5 @@
 from uuid import UUID
 
-import yaml
-
-from dungeon.challenge_keeper import Challenge
-from dungeon.entity import Entity
-
 
 def is_uuid(string):
     try:
@@ -14,42 +9,6 @@ def is_uuid(string):
         return False
 
 
-def room_from_yaml(template):
-    challenge = Challenge(**{"template": yaml.safe_load(template)})
-    return challenge.create_room()
-
-
 def test_helper_is_uuid():
     assert is_uuid("9f4118fe668843a3a1c847552a69b1db")
     assert not is_uuid("banana")
-
-
-def test_helper_room_from_yaml():
-    template = """
-templateType: "visual"
-definitions:
-  p:
-    ai:
-      kind: "singular"
-      action:
-        actionType: "move"
-        direction: "up"
-    actions:
-      move: {}
-room: |-
-  p
-"""
-    room = room_from_yaml(template)
-    assert room.get_entities() == [Entity(**{
-        "position": {
-            "x": 0,
-            "y": 0
-        },
-        "ai": {
-            "kind": "singular",
-            "action": {"actionType": "move", "direction": "up"}
-        },
-        "actions": {
-            "move": {}
-        }
-    })]
