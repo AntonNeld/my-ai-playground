4d35969b7c4884717bc8cccd6fc77621aa89b9bb
Anton Neld <antonherrneld@gmail.com>
2020-10-24 11:40:47 +0200

    Test attack system directly

--- tests/dungeon/systems/test_attack.py
+++ tests/dungeon/systems/test_attack.py
@@ -1,30 +1,48 @@
-from test_utils import room_from_yaml
+import pytest
+
+from dungeon.systems import AttackSystem
+from dungeon.consts import Attack, Position
+from dungeon.entity import Vulnerable
+from dungeon.custom_component_dicts import PositionDict
 
 
-def test_attack():
-    room = room_from_yaml("""
-templateType: "visual"
-definitions:
-  p:
-    looksLike: "player"
-    ai:
-      kind: "singular"
-      action:
-        actionType: "attack"
-        direction: "right"
-    actions:
-      attack: {}
-  e:
-    vulnerable: {}
-  "#": {}
-room: |-
- #pe
-""")
+@pytest.mark.parametrize(
+    "direction,target_x,target_y",
+    [("up", 0, 1), ("down", 0, -1), ("left", -1, 0), ("right", 1, 0)]
+)
+def test_attack_vulnerable(direction, target_x, target_y):
+    actions = {"attacker": Attack(direction=direction)}
+    position_components = PositionDict({
+        "attacker": Position(x=0, y=0),
+        "target": Position(x=target_x, y=target_y)
+    })
+    vulnerable_components = {"target": Vulnerable}
+    system = AttackSystem()
+    removed_entities = system.do_attacks(
+        actions, position_components, vulnerable_components)
+    assert "target" in removed_entities
 
-    room.step()
-    assert len(room.get_entities()) == 2
-    room.step()
-    assert len(room.get_entities()) == 2
-    room.get_entities(looks_like="player")[0].ai.action.direction = "left"
-    room.step()
-    assert len(room.get_entities()) == 2
+
+def test_attack_empty_tile():
+    actions = {"attacker": Attack(direction="right")}
+    position_components = PositionDict({
+        "attacker": Position(x=0, y=0)
+    })
+    vulnerable_components = {}
+    system = AttackSystem()
+    removed_entities = system.do_attacks(
+        actions, position_components, vulnerable_components)
+    assert len(removed_entities) == 0
+
+
+def test_attack_invulnerable():
+    actions = {"attacker": Attack(direction="right")}
+    position_components = PositionDict({
+        "attacker": Position(x=0, y=0),
+        "target": Position(x=1, y=0)
+    })
+    vulnerable_components = {}
+    system = AttackSystem()
+    removed_entities = system.do_attacks(
+        actions, position_components, vulnerable_components)
+    assert len(removed_entities) == 0
