8c4c3ba45a819eb361bc6c82ee76349050afb820
Anton Neld <antonherrneld@gmail.com>
2020-10-24 14:33:38 +0200

    Test drop system directly

--- tests/dungeon/systems/test_drop.py
+++ tests/dungeon/systems/test_drop.py
@@ -1,62 +1,44 @@
-from test_utils import room_from_yaml
-from dungeon.entity import Entity
+from dungeon.entity import Entity, Pickupper, ItemPickup
+from dungeon.systems import DropSystem
+from dungeon.custom_component_dicts import PositionDict
+from dungeon.consts import Drop, Position
 
 
-def test_drop():
-    room = room_from_yaml("""
-templateType: "visual"
-definitions:
-  p:
-    looksLike: "player"
-    ai:
-      kind: "singular"
-      action:
-        actionType: "drop"
-        index: 1
-    pickupper:
-      mode: "action"
-      inventory:
-        - looksLike: "coin"
-          pickup:
-            kind: "item"
-        - looksLike: "evilCoin"
-          pickup:
-            kind: "item"
-    actions:
-      drop: {}
-room: |-
-  p
-""")
-
-    room.step()
-    assert len(room.get_entities()) == 2
-    assert room.get_entities(looks_like="player")[0].pickupper.inventory == [
-        Entity(**{"looksLike": "coin", "pickup": {"kind": "item"}})]
-    room.step()
-    assert len(room.get_entities()) == 2
-    assert room.get_entities(looks_like="player")[0].pickupper.inventory == [
-        Entity(**{"looksLike": "coin", "pickup": {"kind": "item"}})]
+def test_drop_item():
+    system = DropSystem()
+    pickupper_components = {
+        "a": Pickupper(
+            mode="action",
+            inventory=[Entity(pickup=ItemPickup(
+                kind="item"), looksLike="coin")]
+        )
+    }
+    actions = {"a": Drop(index=0)}
+    position_components = PositionDict({"a": Position(x=0, y=0)})
+    created_entities = system.drop_items(
+        pickupper_components, actions, position_components
+    )
+    assert created_entities == [Entity(
+        pickup=ItemPickup(kind="item"),
+        looksLike="coin",
+        position=Position(x=0, y=0)
+    )]
+    assert len(pickupper_components["a"].inventory) == 0
 
 
-def test_drop_autopickup():
-    room = room_from_yaml("""
-templateType: "visual"
-definitions:
-  p:
-    looksLike: "player"
-    ai:
-      kind: "singular"
-      action:
-        actionType: "drop"
-    pickupper:
-      mode: "auto"
-      inventory:
-        - pickup:
-            kind: "item"
-    actions:
-      drop: {}
-room: |-
-  p
-""")
-    room.step()
-    assert len(room.get_entities()) == 2
+def test_drop_nonexistent_index_does_nothing():
+    system = DropSystem()
+    pickupper_components = {
+        "a": Pickupper(
+            mode="action",
+            inventory=[Entity(pickup=ItemPickup(
+                kind="item"), looksLike="coin")]
+        )
+    }
+    actions = {"a": Drop(index=1)}
+    position_components = PositionDict({"a": Position(x=0, y=0)})
+    created_entities = system.drop_items(
+        pickupper_components, actions, position_components
+    )
+    assert created_entities == []
+    assert len(pickupper_components["a"].inventory) == 1
