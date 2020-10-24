ccb9418bf46b75b1f5437d17cabf041efbaf736f
Anton Neld <antonherrneld@gmail.com>
2020-10-24 15:03:57 +0200

    Test movement system directly

--- tests/dungeon/systems/test_movement.py
+++ tests/dungeon/systems/test_movement.py
@@ -1,102 +1,58 @@
 import pytest
 
-from test_utils import room_from_yaml
+from dungeon.systems import MovementSystem
+from dungeon.consts import Move, Position
+from dungeon.custom_component_dicts import PositionDict
+from dungeon.entity import BlocksMovement
 
 
 @pytest.mark.parametrize("direction,x,y",
                          [("up", 0, 1), ("down", 0, -1),
                           ("left", -1, 0), ("right", 1, 0)])
-def test_action_move(direction, x, y):
-    room = room_from_yaml(f"""
-templateType: "visual"
-definitions:
-  p:
-    ai:
-      kind: "singular"
-      action:
-        actionType: "move"
-        direction: "{direction}"
-    actions:
-      move: {{}}
-room: |-
-  p
-""")
-
-    room.step()
-    assert room.get_entities()[0].position.x == x
-    assert room.get_entities()[0].position.y == y
+def test_move_direction(direction, x, y):
+    system = MovementSystem()
+    actions = {"a": Move(direction=direction)}
+    position_components = PositionDict({"a": Position(x=0, y=0)})
+    blocks_movement_components = {}
+    tags_components = {}
+    system.move_entities(actions, position_components,
+                         blocks_movement_components, tags_components)
+    assert position_components["a"] == Position(x=x, y=y)
 
 
 def test_blocks_movement():
-    room = room_from_yaml("""
-templateType: "visual"
-definitions:
-  p:
-    looksLike: "player"
-    ai:
-      kind: "singular"
-      action:
-        actionType: "move"
-        direction: "right"
-    actions:
-      move: {}
-  "#":
-    blocksMovement: {}
-room: |-
-  p#
-""")
-
-    room.step()
-    assert room.get_entities(looks_like="player")[0].position.x == 0
+    system = MovementSystem()
+    actions = {"a": Move(direction="right")}
+    position_components = PositionDict(
+        {"a": Position(x=0, y=0), "b": Position(x=1, y=0)})
+    blocks_movement_components = {"b": BlocksMovement()}
+    tags_components = {}
+    system.move_entities(actions, position_components,
+                         blocks_movement_components, tags_components)
+    assert position_components["a"] == Position(x=0, y=0)
 
 
 def test_blocks_movement_wrong_tags():
-    room = room_from_yaml("""
-templateType: "visual"
-definitions:
-  p:
-    looksLike: "player"
-    ai:
-      kind: "singular"
-      action:
-        actionType: "move"
-        direction: "right"
-    actions:
-      move: {}
-  "~":
-    blocksMovement:
-      passableForTags:
-        - "pass"
-room: |-
-  p~
-""")
-
-    room.step()
-    assert room.get_entities(looks_like="player")[0].position.x == 0
+    system = MovementSystem()
+    actions = {"a": Move(direction="right")}
+    position_components = PositionDict(
+        {"a": Position(x=0, y=0), "b": Position(x=1, y=0)})
+    blocks_movement_components = {
+        "b": BlocksMovement(passableForTags=["pass"])}
+    tags_components = {"a": ["notPass"]}
+    system.move_entities(actions, position_components,
+                         blocks_movement_components, tags_components)
+    assert position_components["a"] == Position(x=0, y=0)
 
 
 def test_blocks_movement_right_tags():
-    room = room_from_yaml("""
-templateType: "visual"
-definitions:
-  p:
-    looksLike: "player"
-    ai:
-      kind: "singular"
-      action:
-        actionType: "move"
-        direction: "right"
-    tags:
-      - "pass"
-    actions:
-      move: {}
-  "~":
-    blocksMovement:
-      passableForTags:
-        - "pass"
-room: |-
-  p~
-""")
-
-    room.step()
-    assert room.get_entities(looks_like="player")[0].position.x == 1
+    system = MovementSystem()
+    actions = {"a": Move(direction="right")}
+    position_components = PositionDict(
+        {"a": Position(x=0, y=0), "b": Position(x=1, y=0)})
+    blocks_movement_components = {
+        "b": BlocksMovement(passableForTags=["pass"])}
+    tags_components = {"a": ["pass"]}
+    system.move_entities(actions, position_components,
+                         blocks_movement_components, tags_components)
+    assert position_components["a"] == Position(x=1, y=0)
